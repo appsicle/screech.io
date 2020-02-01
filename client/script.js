d5714a8aad76252fb2e6f401f226c18ec51a9f07
Albert Zhang <aalbertzhang@gmail.com>
2020-02-01 15:35:46 -0800

    bezier curves

--- client/script.js
+++ client/script.js
@@ -1,66 +1,46 @@
-/* © 2009 ROBO Design
- * http://www.robodesign.ro
- */
+function midPointBtw(p1, p2) {
+    return {
+        x: p1.x + (p2.x - p1.x) / 2,
+        y: p1.y + (p2.y - p1.y) / 2
+    };
+}
 
-// Keep everything in anonymous function, called on window load.
-if(window.addEventListener) {
-    window.addEventListener('load', function () {
-      var canvas, context;
-    
-      // Initialization sequence.
-      function init () {
-        // Find the canvas element.
-        canvas = document.getElementById('imageView');
-        if (!canvas) {
-          alert('Error: I cannot find the canvas element!');
-          return;
-        }
-    
-        if (!canvas.getContext) {
-          alert('Error: no canvas.getContext!');
-          return;
-        }
-        // Get the 2D canvas context.
-        context = canvas.getContext('2d');
-        if (!context) {
-          alert('Error: failed to getContext!');
-          return;
-        }
-        context.canvas.width = window.innerWidth;
-        context.canvas.height = window.innerHeight;
-    
-        // Attach the mousemove event handler.
-        canvas.addEventListener('mousemove', ev_mousemove, false);
-      }
-    
-      // The mousemove event handler.
-      var started = false;
-      function ev_mousemove (ev) {
-        var x, y;
-    
-        // Get the mouse position relative to the canvas element.
-        if (ev.layerX || ev.layerX == 0) { // Firefox
-          x = ev.layerX;
-          y = ev.layerY;
-        } else if (ev.offsetX || ev.offsetX == 0) { // Opera
-          x = ev.offsetX;
-          y = ev.offsetY;
-        }
-    
-        // The event handler works like a drawing pencil which tracks the mouse 
-        // movements. We start drawing a path made up of lines.
-        if (!started) {
-          context.beginPath();
-          context.moveTo(x, y);
-          started = true;
-        } else {
-          context.lineTo(x, y);
-          context.stroke();
-        }
-      }
-    
-      init();
-    }, false); }
-    
-    // vim:set spell spl=en fo=wan1croql tw=80 ts=2 sw=2 sts=2 sta et ai cin fenc=utf-8 ff=unix:
-    
+var el = document.getElementById('imageView');
+var ctx = el.getContext('2d');
+
+ctx.lineJoin = ctx.lineCap = 'round';
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+ctx.lineWidth = 5;
+
+
+var isDrawing, points = [];
+
+el.onmousemove = function (e) {
+    // if (!isDrawing) return;
+
+    points.push({ x: e.clientX, y: e.clientY });
+
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    var p1 = points[0];
+    var p2 = points[1];
+
+    ctx.beginPath();
+    ctx.moveTo(p1.x, p1.y);
+    console.log(points);
+
+    for (var i = 1, len = points.length; i < len; i++) {
+        // we pick the point between pi+1 & pi+2 as the
+        // end point and p1 as our control point
+        var midPoint = midPointBtw(p1, p2);
+        ctx.quadraticCurveTo(p1.x, p1.y, midPoint.x, midPoint.y);
+        p1 = points[i];
+        p2 = points[i + 1];
+    }
+    // Draw last line as a straight line while
+    // we wait for the next point to be able to calculate
+    // the bezier control point
+    ctx.lineTo(p1.x, p1.y);
+    ctx.stroke();
+};
