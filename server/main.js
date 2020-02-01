eec6f81835f65ad415b20b9d5b46bf19c170ac2c
Albert Zhang <aalbertzhang@gmail.com>
2020-02-01 14:28:32 -0800

    add drawing functionality

--- server/main.js
+++ server/main.js
@@ -1,6 +1,9 @@
-var app = require('express')();
+var express = require('express')
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/../client'));
 
 app.get('/', function (req, res) {
     res.sendFile('/client/index.html', { root : '../'});
