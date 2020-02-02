7a05a95d43f9726efdb90e66adf3b976b56c2cda
Leon <leonlin.48@gmail.com>
2020-02-01 22:23:18 -0800

    adding colors and randomizing images

--- client/myapp/src/Components/CanvasContainer.js
+++ client/myapp/src/Components/CanvasContainer.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import Canvas from './Canvas'
+
 import panda from '../images/panda.png';
 import gary from '../images/gary-snail.jpeg';
+import circle from '../images/circle.png';
+import square from '../images/square.png';
+import triangle from '../images/triangle.png';
+import shrek from '../images/shrek.png';
+import flower from '../images/flower.png';
+
+let images = [panda, gary, circle, square, triangle, shrek, flower];
+let image = images[Math.floor(Math.random()*images.length)];
 
 
 class CanvasContainer extends Component {
@@ -27,8 +36,16 @@ class CanvasContainer extends Component {
                         <div className="color green" onClick={() => this.chooseColor('green')}></div>
                         <div className="color blue" onClick={() => this.chooseColor('blue')}></div>
                         <div className="color yellow" onClick={() => this.chooseColor('darkorange')}></div>
+                        <div className="color brown" onClick={() => this.chooseColor('brown')}></div>
+                        <div className="color purple" onClick={() => this.chooseColor('purple')}></div>
+                        <div className="color gold" onClick={() => this.chooseColor('gold')}></div>
+                        <div className="color teal" onClick={() => this.chooseColor('teal')}></div>
+                        <div className="color pink" onClick={() => this.chooseColor('pink')}></div>
+                        <div className="color fuchia" onClick={() => this.chooseColor('fuchia')}></div>
+                        <div className="color dimgray" onClick={() => this.chooseColor('dimgray')}></div>
                     </div>
-                    <img src={gary}></img>
+                    <p>Try to draw this!</p>
+                    <img src={image}></img>
                 </div>
                 <Canvas className="canvas" color={this.state.color} />
             </div>
