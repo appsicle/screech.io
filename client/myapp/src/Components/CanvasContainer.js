2299a0bea3d055af832db44006960c3ea2255c12
Albert Zhang <aalbertzhang@gmail.com>
2020-02-02 03:53:38 -0800

    merge

--- client/myapp/src/Components/CanvasContainer.js
+++ client/myapp/src/Components/CanvasContainer.js
@@ -10,9 +10,11 @@ import square from '../images/square.png';
 import triangle from '../images/triangle.png';
 import shrek from '../images/shrek.png';
 import flower from '../images/flower.png';
+import Timer from 'react-compound-timer';
 
 let images = [panda, gary, circle, square, triangle, shrek, flower];
 let image = images[Math.floor(Math.random()*images.length)];
+const playTime = 30000;
 const Mp3Recorder = new MicRecorder({ bitRate: 128 });
 
 class CanvasContainer extends Component {
@@ -72,41 +74,47 @@ class CanvasContainer extends Component {
         this.setState({stopped: true});
     };
 
-    render() {
-        return (
-            <div className="game-container">
-                <div>
-                    <div className="color-header">Pick a color</div>
-                    <div className="colors">
-                        <div className="color black" onClick={() => this.chooseColor('black')}></div>
-                        <div className="color red" onClick={() => this.chooseColor('red')}></div>
-                        <div className="color green" onClick={() => this.chooseColor('green')}></div>
-                        <div className="color blue" onClick={() => this.chooseColor('blue')}></div>
-                        <div className="color yellow" onClick={() => this.chooseColor('darkorange')}></div>
-                        <div className="color brown" onClick={() => this.chooseColor('brown')}></div>
-                        <div className="color purple" onClick={() => this.chooseColor('purple')}></div>
-                        <div className="color gold" onClick={() => this.chooseColor('gold')}></div>
-                        <div className="color teal" onClick={() => this.chooseColor('teal')}></div>
-                        {/* <div className="color pink" onClick={() => this.chooseColor('pink')}></div>
-                        <div className="color fuchia" onClick={() => this.chooseColor('fuchia')}></div>
-                        <div className="color dimgray" onClick={() => this.chooseColor('dimgray')}></div> */}
-                    </div>
-                    <p>Try to draw this!</p>
-                    <img src={image}></img>
-                    {/*<button onClick={this.start} disabled={this.state.isRecording}>
-                      Record
-                    </button>*/}
-                    <Table className="table" username={this.props.username}></Table>
-                    <Button onClick={this.stop} disabled={!this.state.isRecording}>
-                      Done
-                    </Button>
+  render() {
+    return (<div className="game-container">
+      <div>
+        <Timer
+          initialTime={playTime}
+          direction="backward"
+          checkpoints={[
+            {
+              time: 0,
+              callback: this.stopGame,
+            }
+          ]}
+        >
+          <div className="timer">
+            <Timer.Seconds /> seconds left
+          </div>
+        </Timer>
+        <div className="color-header">Pick a color</div>
+        <div className="colors">
+          <div className="color black" onClick={() => this.chooseColor('black')}></div>
+          <div className="color red" onClick={() => this.chooseColor('red')}></div>
+          <div className="color green" onClick={() => this.chooseColor('green')}></div>
+          <div className="color blue" onClick={() => this.chooseColor('blue')}></div>
+          <div className="color yellow" onClick={() => this.chooseColor('darkorange')}></div>
+          <div className="color brown" onClick={() => this.chooseColor('brown')}></div>
+          <div className="color purple" onClick={() => this.chooseColor('purple')}></div>
+          <div className="color gold" onClick={() => this.chooseColor('gold')}></div>
+          <div className="color teal" onClick={() => this.chooseColor('teal')}></div>
+        </div>
+        <p>Try to draw this!</p>
 
-                </div>
-                <Canvas className="canvas" color={this.state.color} stopped={this.state.stopped} audioPlayer={<audio src={this.state.blobURL} controls="controls" />}/>
-            </div>
-
-        );
-    }
+        <img src={image}></img>
+        <Table className="table" username={this.props.username}></Table>
+        {/* <Button onClick={this.stop} disabled={!this.state.isRecording}>
+          Done
+      </Button> */}
+        <audio src={this.state.blobURL} controls="controls" />
+      </div>
+      <Canvas className="canvas" color={this.state.color} />
+    </div>);
+  }
 }
 
 export default CanvasContainer;
